e3bc4f1071713edeaff016d89c4ff3315280a5c0
Rafael Castillo <savoytruffle321@gmail.com>
2015-05-08 12:57:49 -0700

    Hagamos esto equipo

---
+++ baraja.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef int Carta
+
+int main(){
+	//xd
+}
+
+class Baraja{
+	vector<Carta> cartas;
+
+	Baraja(Carta numCartas=207){
+		for(Carta i = 0; i < numCartas; i++)
+			cartas.push_back(i);
+	}
+
+	public:
+		void Revolver(){
+			random_shuffle(cartas.begin(), cartas.end());
+		}
+
+		Carta Jalar(){
+			Carta carta = cartas[0];
+			cartas.erase(cartas.begin());
+			return carta;
+		}
+};
+
+class Mano{
+	
+}
+
+
